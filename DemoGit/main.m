3b43ed3c9c85b3df90559df1ab150504a0e819cd
RoneLozaAliaga <rone.loza@tismartperu.com>
2014-01-08 18:41:59 -0500

    Initial Commit

---
+++ DemoGit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DemoGit
+//
+//  Created by Rone Loza on 1/8/14.
+//  Copyright (c) 2014 Tismart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
