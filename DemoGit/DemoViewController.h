4e1b7d68eac45f15e4ca9ff3931943149a6ff88a
RoneLozaAliaga <rone.loza@tismartperu.com>
2014-01-08 18:51:28 -0500

    Creacion de nuevo controlador para verificar cambios

---
+++ DemoGit/DemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  DemoViewController.h
+//  DemoGit
+//
+//  Created by Rone Loza on 1/8/14.
+//  Copyright (c) 2014 Tismart. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DemoViewController : UIViewController
+
+@end
---
+++ DemoGit/DemoViewController.m
@@ -0,0 +1,13 @@
+//
+//  DemoViewController.m
+//  DemoGit
+//
+//  Created by Rone Loza on 1/8/14.
+//  Copyright (c) 2014 Tismart. All rights reserved.
+//
+
+#import "DemoViewController.h"
+
+@implementation DemoViewController
+
+@end
